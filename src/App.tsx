c6e733673beb3bf5986b7d70d432cfc8f2187d3f
rgaibor17 <108085022+rgaibor17@users.noreply.github.com>
2024-06-19 14:14:07 -0500

    Guía 12. Componentes.
    Componente MUI Paper y Typography.
    Componente MUI Card.
    Componente MUI Table.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@
 import './App.css'
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import BasicTable from './components/BasicTable';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -18,8 +20,35 @@ function App() {
       <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
     </Grid>*/
 
-    <Grid xs={6} md={4} lg={2}>
-      <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+    <Grid container spacing={5}>
+      <Grid xs={12} sm={2} md={2} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={12} sm={2} md={2} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={12} sm={2} md={2} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={12} sm={2} md={2} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={12} sm={2} md={2} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={12} sm={2} md={2} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+
+      <Grid xs={12} sm={2} md={2} lg={2}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+        <br/>
+        <Summary/>
+      </Grid>
+
+      <Grid xs={12} md={10} lg={10}>
+	       <BasicTable />
+      </Grid>
     </Grid>
   )
 }
--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -1,3 +1,6 @@
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+
 interface Config {
     title?: String;
     subtitle?: String;
@@ -6,10 +9,22 @@ interface Config {
 
 export default function Indicator(config: Config) {
     return (
-       	<>
-            {config.title}<br/>
-            {config.value.toString()}<br/>
-            {config.subtitle}
-        </> 
+        <Paper
+            sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column'
+                }}
+        >
+            <Typography component="h2" variant="h6" color="primary" gutterBottom>
+                {config.title} 
+            </Typography>
+            <Typography component="p" variant="h4">
+                {config.value.toString()}
+            </Typography>
+            <Typography color="text.secondary" sx={{ flex: 1 }}>
+                {config.subtitle}
+            </Typography>
+        </Paper>
     )
 }
